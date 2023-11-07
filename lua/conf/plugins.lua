7ce102b4aa62815c75a41c8c9674f6f17510771f
ncob000 <nicolasbetancur250@gmail.com>
2023-11-07 00:11:39 -0500

    Adicion de nuevos plugins

--- lua/conf/plugins.lua
+++ lua/conf/plugins.lua
@@ -1,50 +1,67 @@
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
-   
-    use 'wbthomason/packer.nvim'
-    use "rebelot/kanagawa.nvim"
-    use 'nvim-lualine/lualine.nvim'
-    use 'nvim-tree/nvim-web-devicons'
-    use "lukas-reineke/indent-blankline.nvim"
-    use "folke/trouble.nvim"
-    use 'folke/neodev.nvim'
-    use 'marko-cerovac/material.nvim'
-    use 'm4xshen/autoclose.nvim'
+    use 'wbthomason/packer.nvim'                                    -- packer manage itself
+    use "rebelot/kanagawa.nvim"                                     -- colorscheme
+    use 'nvim-lualine/lualine.nvim'                                 -- status line
+    use 'nvim-tree/nvim-web-devicons'                               -- icons
+    use "lukas-reineke/indent-blankline.nvim"                       -- color identation
+    use "folke/trouble.nvim"                                        -- show problems whit the code
+    use 'folke/neodev.nvim'                                         -- extended lib for neovim
+    use 'marko-cerovac/material.nvim'                               -- another colorscheme
+    use 'm4xshen/autoclose.nvim'                                    -- autoclose (){}
+    use 'windwp/nvim-ts-autotag'                                    -- HTML help
+    use 'nvim-treesitter/nvim-treesitter-context'                   -- treesitter plugin
+    use 'RRethy/vim-illuminate'                                     -- resalt equals words
+    use "stevearc/conform.nvim"                                     -- format code
+    use "folke/twilight.nvim"                                       -- decolirezed unused code
+    use 'lewis6991/gitsigns.nvim'                                   -- git sings changes
+    use 'sindrets/winshift.nvim'                                    -- manage windows
+    use 'CRAG666/code_runner.nvim'                                  -- Run code
 
-     use{
-    	'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        requires =  {
-            {'nvim-lua/plenary.nvim'},
-            {'nvim-telescope/telescope-file-browser.nvim'},
+    use { 'michaelb/sniprun', run = 'sh ./install.sh' }             -- Run parts of the code
+    use { "mcchrish/zenbones.nvim", requires = "rktjmp/lush.nvim" } -- Another colorscheme
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }    -- just treesitter
+
+    use {
+        'kkoomen/vim-doge',
+        run = ':call doge#install()',
+    }
+
+    use {
+        "akinsho/toggleterm.nvim",
+        tag = '*',
+        config = function()
+            require("toggleterm").setup()
+        end,
+    }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = {
+            {
+                'nvim-lua/plenary.nvim',
+                "nvim-telescope/telescope-file-browser.nvim"
+            }
         }
-	}
-	use {
-        "mcchrish/zenbones.nvim",
-    		requires = "rktjmp/lush.nvim"
-	}
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-            -- Mason setup
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+    }
+
+
+    use {
+        'VonHeikemen/lsp-zero.nvim', -- lsp config
+        branch = 'v3.x',
+        requires = {
+            { 'williamboman/mason.nvim' },           -- Mason (LSP management)
+            { 'williamboman/mason-lspconfig.nvim' }, -- Mason configuration
+            { 'neovim/nvim-lspconfig' },             -- LSP config
+            { 'hrsh7th/nvim-cmp' },                  -- Autocomplenion
+            { 'hrsh7th/cmp-nvim-lsp' },              -- LSP autocomplenion
+            { 'L3MON4D3/LuaSnip' },                  -- snips for LSP
+        }
+    }
     use({
-        "iamcco/markdown-preview.nvim",
+        "iamcco/markdown-preview.nvim", -- markdown previow plugin
         run = "cd app && npm install",
-        setup = function() vim.g.mkdp_filetypes = { "markdown" } end, 
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
         ft = { "markdown" },
     })
 end)
