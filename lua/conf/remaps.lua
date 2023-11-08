14fb7f1c23a99f5fe8f46b31c7c4639eac890fa8
ncob000 <nicolasbetancur250@gmail.com>
2023-11-08 18:05:03 -0500

    Add new remaps

--- lua/conf/remaps.lua
+++ lua/conf/remaps.lua
@@ -2,11 +2,11 @@
 vim.g.mapleader = " "
 
 -- Editor keymaps --
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)     -- file out
-vim.keymap.set("n", "<leader>s", vim.cmd.w)       -- save file
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- file out
+vim.keymap.set("n", "<leader>s", vim.cmd.w) -- save file
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("v", "<Up>", ":m '<-2<CR>gv=gv") -- Move text up
 vim.keymap.set("v", "<Down>", ":m '>+1<CR>gv=gv") -- Move text down
-vim.keymap.set("v", "<Up>", ":m '<-2<CR>gv=gv")   -- Move text up
 vim.keymap.set("n", "<leader>c", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Trouble keymaps --
@@ -18,19 +18,18 @@ vim.keymap.set("n", "<leader>tr", ":TroubleRefresh <CR>")
 vim.keymap.set("n", "<leader>ot", ":ToggleTerm <CR>")
 
 -- Telescope keymaps --
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})     -- project files --
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") }) -- find files for his content --
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {}) -- project files --
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") }) -- find files for his content --
 end)
-vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<CR>')
-
+vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
 
 -- Window manipulation --
 vim.keymap.set("n", "<leader>sp", ":sp <CR>")
 vim.keymap.set({ "n", "i", "v" }, "<Esc>w", ":WinShift <CR>")
-vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]])   -- make the window biger vertically
-vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]])   -- make the window smaller vertically
+vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
 vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
 vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
 
