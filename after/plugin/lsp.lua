34ce0322994acdd93bac523af0ec0518d815b812
ncob000 <nicolasbetancur250@gmail.com>
2023-12-06 16:39:50 -0500

    IMprove

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,52 +1,90 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- see :help lsp-zero-guide:integrate-with-mason-nvim
 -- to learn how to use mason.nvim with lsp-zero
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    handlers = {
-        lsp_zero.default_setup,
-    }
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
+require("lsp_signature").setup({
+	bind = true, -- This is mandatory, otherwise border config won't get registered.
+	handler_opts = {
+		border = "rounded",
+	},
+})
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 cmp.setup({
-    preselect = 'item',
-    completion = {
-        completeopt = 'menu,menuone,noinsert'
-    },
+	preselect = "item",
 
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "orgmode" },
+	}, {
+		{ name = "buffer" },
+	}),
+
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
+
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 })
 
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
 
-local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
+
+local cmp_action = require("lsp-zero").cmp_action()
 
 cmp.setup({
-    mapping = cmp.mapping.preset.insert({
-        -- `Enter` key to confirm completion
-        ['<TAB>'] = cmp.mapping.confirm({ select = false }),
+	mapping = cmp.mapping.preset.insert({
+		-- `Enter` key to confirm completion
+		["<TAB>"] = cmp.mapping.confirm({ select = false }),
 
-        -- Ctrl+Space to trigger completion menu
-        ['<C-Space>'] = cmp.mapping.complete(),
+		-- Ctrl+Space to trigger completion menu
+		["<C-Space>"] = cmp.mapping.complete(),
 
-        -- Navigate between snippet placeholder
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+		-- Navigate between snippet placeholder
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 
-        -- Scroll up and down in the completion documentation
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    })
+		-- Scroll up and down in the completion documentation
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
 })
+
+require("luasnip.loaders.from_vscode").lazy_load()
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,16 +1,23 @@
-require('telescope').setup{
-  defaults = {
-        theme = "zenbones",
-  }, 
-  extensions = {
-      file_browser = {
-      -- disables netrw and use telescope-file-browser in its place
-            hijack_netrw = true,
-            mappings = {
-            },
-        
-        },
-    }
-}
+require("telescope").setup({
+	defaults = {
+		theme = "zenbones",
+	},
+	extensions = {
+		file_browser = {
+			-- disables netrw and use telescope-file-browser in its place
+			hijack_netrw = true,
+			mappings = {},
+		},
 
-require("telescope").load_extension "file_browser"
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case",
+		},
+	},
+})
+
+require("telescope").load_extension("frecency")
+require("telescope").load_extension("file_browser")
+require("telescope").load_extension("fzf")
